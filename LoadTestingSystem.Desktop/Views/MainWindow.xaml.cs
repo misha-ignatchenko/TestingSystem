5262ff166f8546abe41fb5cc8bbef392e6de2660
Mykhailo <mcmish133@gmail.com>
2026-07-16 15:37:02 +0300

    Implement pagination in desktop application

--- LoadTestingSystem.Desktop/Views/MainWindow.xaml.cs
+++ LoadTestingSystem.Desktop/Views/MainWindow.xaml.cs
@@ -7,6 +7,10 @@ namespace LoadTestingSystem.Desktop
 {
     public partial class MainWindow : Window
     {
+        private int _currentPage = 1;
+        private const int PageSize = 10;
+        private int _totalPages;
+
         private readonly LoadTestResultService _loadTestResultService = new();
         public MainWindow()
         {
@@ -15,9 +19,15 @@ namespace LoadTestingSystem.Desktop
 
         private async Task LoadDataAsync()
         {
-            var result = await _loadTestResultService.GetAllAsync();
+            var result = await _loadTestResultService.GetAllAsync(_currentPage, PageSize);
 
             ResultsDataGrid.ItemsSource = result.Items;
+            _totalPages = result.TotalPages;
+
+            PageInfoTextBlock.Text = _totalPages == 0 ? "0 / 0" : $"{_currentPage} / {_totalPages}";
+
+            PreviousButton.IsEnabled = _currentPage > 1;
+            NextButton.IsEnabled = _currentPage < _totalPages;
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
@@ -83,6 +93,20 @@ namespace LoadTestingSystem.Desktop
             {
                 MessageBox.Show("Не вдалося видалити запис.");
             }
+        }
+
+        private async void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            _currentPage++;
+
+            await LoadDataAsync();
+        }
+
+        private async void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            _currentPage--;
+
+            await LoadDataAsync();
         }
     }
 }
